b2bf8081f2422149142b1afed0ae125937bc1172
Antonia Cerda <antoniacerdag@gmail.com>
2017-07-06 17:32:13 -0400

    pagination

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,11 +1,35 @@
 $(document).ready(function() {
-	var dibujarGifs = function(data){
-		var poke = "";
+	var imprimirPokemon = function(data){
+		var gif = "";
 		var url = "";
 		data.forEach(function(element){
-			poke = element.images.downsized_large.url;
+			gif = element.images.downsized_large.url;
 			url = element.bitly_gif_url;
-			$("#pokemon").append(armarTemplatePokemon(gif, url));
+			$("#elementos").append(armarTemplate(gif, url));
 		})
 	}	
+
+	var armarTemplatePoke = function(gif,url){
+		var t = "<div class='pokemon'><img src'" + gif + "'/><a href='" + url + "'>Ver más</a></div>"
+		return t;
+	}
+	var ajaxPoke = function(poke){
+		$.ajax({
+			url: 'http://pokeapi.co/api/v2/pokedex/1/',
+			type: 'GET',
+			datatype: 'json',
+			data: {
+				q : gif,
+				api_key: 'dc6zaTOxFJmzC'
+			}
+		})
+		.done(function(response){
+			console.log(response);
+			imprimirPokemon(response.data);
+		})
+		.fail(function(){
+			console.log("error");
+		})
+	}
+	
 });
