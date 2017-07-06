4e1055a336d6548dabc0cabf0cb8b1cd10806bf0
Antonia Cerda <antoniacerdag@gmail.com>
2017-07-06 17:11:12 -0400

    llamando api

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,1 +1,11 @@
-main.js
+$(document).ready(function() {
+	var dibujarGifs = function(data){
+		var poke = "";
+		var url = "";
+		data.forEach(function(element){
+			poke = element.images.downsized_large.url;
+			url = element.bitly_gif_url;
+			$("#pokemon").append(armarTemplatePokemon(gif, url));
+		})
+	}	
+});
