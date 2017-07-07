201f6205edce90840428ddec257b7d84495d9efd
Antonia Cerda <antoniacerdag@gmail.com>
2017-07-07 11:29:03 -0400

    imprime nombre y estilo

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,33 +1,13 @@
 $(document).ready(function() {
-	var mostrarPokemon = function(data){
-		//var pokeID = data.national_id;
-	    var pokeName = data.name;
-	    var pokeType1 = data.types[0].name;
-	    if (data.types.length == 2) {
-	    	var pokeType2 = data.types[1].name;
-	    }
-	    else var pokeType2 = null;
-	    var descriptionURI = "http://pokeapi.co" + data.descriptions[0].resource_uri;
-	    var pokeDescription = "";
-	    data.forEach(function(ele){
-	    	$("#pokemon").append(templatePokemon())
-	    })
-	}
-	var templatePokemon = function(){
-		var pokemones = "<div class='elemento'>" + pokeName + pokeType1 + pokeType2 + "</div>";
-		return pokemones;
-	}
-
 	$.ajax({
 		url: 'http://pokeapi.co/api/v2/pokemon',
 		type: 'GET',
 		dataType: 'JSON',
-		data: {"limit": '811'},
+		data: {"limit": '20'},
 	})
-
-	
 	.done(function(respuesta) {
 		console.log(respuesta);
+		mostrarPokemon(respuesta.results);
 	})
 	.fail(function() {
 		console.log("error");
@@ -35,5 +15,27 @@ $(document).ready(function() {
 	.always(function() {
 		console.log("complete");
 	});
+
+	var mostrarPokemon = function(data){
+		//var pokeID = data.national_id;
+		
+	    var pokeDescription = "";
+	    data.forEach(function(ele){
+	    	var pokeName = ele.name;
+	    	
+	    	$("#pokemon").append("<div class='pokelinea1'>"+pokeName+"</div> ");
+	    })
+	}
+
+	//variable para formato de impreción en HTML
+	/*var templatePokemon = function(pokeURL2){
+		var pokemones = "<div class='elemento'>" + pokeURL2 + pokeName+ "</div>";
+		return pokemones;
+	}*/
+
+	
+
+	
+	
 	
 });
