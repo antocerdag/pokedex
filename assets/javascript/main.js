bf2026f19c8af380699e2e78529df24cd259bde3
Antonia Cerda <antoniacerdag@gmail.com>
2017-07-07 10:55:30 -0400

    jquery

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,35 +1,39 @@
 $(document).ready(function() {
-	var imprimirPokemon = function(data){
-		var gif = "";
-		var url = "";
-		data.forEach(function(element){
-			gif = element.images.downsized_large.url;
-			url = element.bitly_gif_url;
-			$("#elementos").append(armarTemplate(gif, url));
-		})
-	}	
+	var mostrarPokemon = function(data){
+		//var pokeID = data.national_id;
+	    var pokeName = data.name;
+	    var pokeType1 = data.types[0].name;
+	    if (data.types.length == 2) {
+	    	var pokeType2 = data.types[1].name;
+	    }
+	    else var pokeType2 = null;
+	    var descriptionURI = "http://pokeapi.co" + data.descriptions[0].resource_uri;
+	    var pokeDescription = "";
+	    data.forEach(function(ele){
+	    	$("#pokemon").append(templatePokemon())
+	    })
+	}
+	var templatePokemon = function(){
+		var pokemones = "<div class='elemento'>" + pokeName + pokeType1 + pokeType2 + "</div>";
+		return pokemones;
+	}
 
-	var armarTemplatePoke = function(gif,url){
-		var t = "<div class='pokemon'><img src'" + gif + "'/><a href='" + url + "'>Ver más</a></div>"
-		return t;
-	}
-	var ajaxPoke = function(poke){
-		$.ajax({
-			url: 'http://pokeapi.co/api/v2/pokedex/1/',
-			type: 'GET',
-			datatype: 'json',
-			data: {
-				q : gif,
-				api_key: 'dc6zaTOxFJmzC'
-			}
-		})
-		.done(function(response){
-			console.log(response);
-			imprimirPokemon(response.data);
-		})
-		.fail(function(){
-			console.log("error");
-		})
-	}
+	$.ajax({
+		url: 'http://pokeapi.co/api/v2/pokemon',
+		type: 'GET',
+		dataType: 'JSON',
+		data: {"limit": '811'},
+	})
+
+	
+	.done(function(respuesta) {
+		console.log(respuesta);
+	})
+	.fail(function() {
+		console.log("error");
+	})
+	.always(function() {
+		console.log("complete");
+	});
 	
 });
